c06fa1f8d2a3bff8684479c8ed55022a7e384834
Arthur D Afonseca <arthur.dafonseca89@gmail.com>
2023-08-28 10:08:31 -0300

    feat: add config file

---
+++ src/config/config.go
@@ -0,0 +1,37 @@
+package config
+
+import (
+	"fmt"
+	"log"
+	"os"
+	"strconv"
+
+	"github.com/joho/godotenv"
+)
+
+var (
+	StringConexaoBanco = ""
+	Porta              = 0
+	BinURL             = ""
+)
+
+func Carregar() {
+	var erro error
+
+	if erro = godotenv.Load(); erro != nil {
+		log.Fatal(erro)
+	}
+
+	Porta, erro = strconv.Atoi(os.Getenv("API_PORT"))
+	if erro != nil {
+		Porta = 9000
+	}
+
+	StringConexaoBanco = fmt.Sprintf("%s:%s@/%s?charset=utf8&parseTime=True&loc=Local",
+		os.Getenv("DB_USER"),
+		os.Getenv("DB_PASSWORD"),
+		os.Getenv("DB_NAME"),
+	)
+
+	BinURL = os.Getenv("BIN_URL")
+}
